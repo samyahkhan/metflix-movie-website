6743c507fe1f06dc8e410d353482d7cc244bc4d1
samyahkhan <120241476+samyahkhan@users.noreply.github.com>
2024-07-04 22:30:15 +0530

    Updated API endpoint URLs to use HTTPS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 
 // 65cd6a37
 
-const API_URL = "http://www.omdbapi.com?apikey=c032e2d7";
+const API_URL = "https://www.omdbapi.com?apikey=c032e2d7";
 
 const movie1 = {
   Title: "Spiderman",
